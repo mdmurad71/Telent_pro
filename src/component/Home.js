fd5c3c7197002008f9ad6de245c91f3c8e554456
mdmurad71 <mdmurad1685@gmail.com>
2023-06-12 03:36:06 -0700

    fourth commit on content

--- src/component/Home.js
+++ src/component/Home.js
@@ -36,7 +36,7 @@ const Home = () => {
                 <h5 className=''>Storage</h5>
                 </div>
                 <div className='content_desc mt-3'>
-                <p classname=''>We pick up, store and deliver</p>              
+                <p classname=''>24/7 Access Securd Storage Unit</p>              
                </div>
                <div className='content_link mt-3'>
                <a><Link to='#'>Learn more</Link></a>
@@ -50,7 +50,7 @@ const Home = () => {
                 <h5 className=''>Storage</h5>
                 </div>
                 <div className='content_desc mt-3'>
-                <p classname=''>We pick up, store and deliver</p>              
+                <p classname=''>Store offsite and manage online </p>              
                </div>
                <div className='content_link mt-3'>
                <a><Link to='#'>Learn more</Link></a>
@@ -65,13 +65,61 @@ const Home = () => {
                 <h5 className=''>Merchendise</h5>
                 </div>
                 <div className='content_desc mt-3'>
-                <p classname=''>We pick up, store and deliver</p>              
+                <p classname=''>Send Swags to Us and click for returns</p>              
                </div>
                <div className='content_link mt-3'>
                <a><Link to='#'>Learn more</Link></a>
                </div>
 
                 </div>
+            </div>
+
+
+            <div className='row mt-5'>
+                <div className='col-lg-3 col-md-3 col-sm-6'>
+                <img src="assets/images/ecommerce.jpg" className="d-flex-justify-content-center  door_img card-img-top" alt="..." />
+                <div className='door_desc d-flex-justify-content-center'>
+                <h5 className='mt-3'>Retail And</h5>
+                <h5 className=''>E-commerce</h5>
+                </div>
+                <div className='content_desc mt-3'>
+                <p classname=''>We'll store, receive, pick, pack and ship</p>              
+               </div>
+               <div className='content_link mt-3'>
+               <a><Link to='#'>Learn more</Link></a>
+               </div>
+                </div>
+
+                <div className='col-lg-3 col-md-3 col-sm-6 mt-5'>
+                <img src="assets/images/co-working.jpg" className="d-flex-justify-content-center  door_img card-img-top" alt="..." />
+                <div className='door_desc d-flex-justify-content-center'>
+                <h5 className='mt-5'>Co-Working and</h5>
+                <h5 className=''>Co-wirehouse</h5>
+                </div>
+                <div className='content_desc mt-4'>
+                <p classname=''>work, store, operate and ship all in one place</p>              
+               </div>
+               <div className='content_link mt-3'>
+               <a><Link to='#'>Learn more</Link></a>
+               </div>
+                </div>
+
+                <div className='col-lg-3 col-md-3 col-sm-6 mt-4'>
+                <img src="assets/images/office.jpg" className="d-flex-justify-content-center  door_img card-img-top" alt="..." />
+                <div className='door_desc d-flex-justify-content-center'>
+                <h5 className='mt-5'>Office With</h5>
+                <h5 className=''>Storage</h5>
+                </div>
+                <div className='content_desc mt-3'>
+                <p classname=''>Office Space With Storage space</p>              
+               </div>
+               <div className='content_link mt-3'>
+               <a><Link to='#'>Learn more</Link></a>
+               </div>
+
+                </div>
+
+
             </div>
 
         </div>
